ea9e25dbc470bd989580a58bfe323939527dd8dc
David Pilo <davidpilo@gmail.com>
2014-04-14 19:55:36 +0200

    moved all vim setup to another script

--- mac_basic_setup.sh
+++ mac_basic_setup.sh
@@ -15,75 +15,8 @@ git config --global alias.st status
 git config --global user.name "David Pilo"
 git config --global user.email davidpilo@gmail.com
 
-
-# Install pathogen.vim
-cd $HOME
-mkdir .vim
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-
-# Install tagbar
-cd ~/.vim/bundle
-git clone git://github.com/majutsushi/tagbar
-
-# Install vim plugin
-cd ~/.vim/bundle
-git clone git://github.com/tpope/vim-surround.git
-
-# Install vim nerdtree
-cd ~/.vim/bundle
-git clone https://github.com/scrooloose/nerdtree.git
-
-# Install matchit plugin
-cd ~/.vim/bundle
-mkdir matchit
-cd matchit
-wget http://www.vim.org/scripts/download_script.php?src_id=8196 -O matchitzip
-unzip matchitzip
-rm matchitzip
-
-# Install vim multiple curlsors plugin
-cd ~/.vim/bundle
-git clone https://github.com/terryma/vim-multiple-cursors.git
-
-# Install fugitive plugin 
-cd ~/.vim/bundle
-git clone git://github.com/tpope/vim-fugitive.git
-
-# Install autocompletepop and L9 vim plugins
-cd $HOME
-wget https://bitbucket.org/ns9tks/vim-autocomplpop/get/tip.zip
-unzip tip.zip -d tip
-cd tip
-cd *auto*
-cp -R * ~/.vim/
-cd $HOME
-rm -R tip/
-rm tip.zip
-cd $HOME
-wget https://bitbucket.org/ns9tks/vim-l9/get/tip.zip
-unzip tip.zip -d tip
-cd tip
-cd *l9*
-cp -R * ~/.vim/
-cd $HOME
-rm -R tip/
-rm tip.zip
-
-# Install solarized color scheme for vim
-cd ~/.vim/bundle
-git clone git://github.com/altercation/vim-colors-solarized.git
-
-#slime for copy paste to another screen 
-cd ~/.vim/bundle
-git clone git://github.com/jpalardy/vim-slime.git
-
-# Install vim ultisnips plugin
-cd ~/.vim/bundle/
-git clone https://github.com/SirVer/ultisnips.git
-git clone https://github.com/honza/vim-snippets.git
-echo -e "snippet php \"php\"\n<?php \${1} ?>\nendsnippet\n" >> ~/.vim/bundle/vim-snippets/UltiSnips/php.snippets # Add missing basic insert php snippet
-echo "extends html" >> ~/.vim/bundle/vim-snippets/UltiSnips/php.snippets #add html snippets to php files
+# Install all vim plugins:
+./vim_setup.sh | sh
 
 ##############################
 # DOTFILES 
--- setup.sh
+++ setup.sh
@@ -18,78 +18,9 @@ git config --global alias.st status
 git config --global user.name "David Pilo"
 git config --global user.email davidpilo@gmail.com
 
-# Install pathogen.vim
-cd $HOME
-mkdir .vim
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+# Install all vim plugins:
+./vim_setup.sh | sh
 
-# Install tagbar
-cd ~/.vim/bundle
-git clone git://github.com/majutsushi/tagbar
-
-
-# Install surround vim plugin
-cd ~/.vim/bundle
-git clone git://github.com/tpope/vim-surround.git
-
-# Install vim nerdtree
-cd ~/.vim/bundle
-git clone https://github.com/scrooloose/nerdtree.git
-
-# Install matchit plugin
-cd ~/.vim/bundle
-mkdir matchit
-cd matchit
-wget http://www.vim.org/scripts/download_script.php?src_id=8196 -O matchitzip
-unzip matchitzip
-rm matchitzip
-
-# Install vim multiple curlsors plugin
-cd ~/.vim/bundle
-git clone https://github.com/terryma/vim-multiple-cursors.git
-
-# Install fugitive plugin 
-cd ~/.vim/bundle
-git clone git://github.com/tpope/vim-fugitive.git
-
-# Install autocompletepop and L9 vim plugins
-cd $HOME
-wget https://bitbucket.org/ns9tks/vim-autocomplpop/get/tip.zip
-unzip tip.zip -d tip
-cd tip
-cd *auto*
-cp -R * ~/.vim/
-cd $HOME
-rm -R tip/
-rm tip.zip
-cd $HOME
-wget https://bitbucket.org/ns9tks/vim-l9/get/tip.zip
-unzip tip.zip -d tip
-cd tip
-cd *l9*
-cp -R * ~/.vim/
-cd $HOME
-rm -R tip/
-rm tip.zip
-
-# Install molokai color scheme for vim
-# cd ~/.vim
-# git clone https://github.com/tomasr/molokai.git 
-# mkdir colors
-# cp molokai/colors/molokai.vim colors/
-# rm -rf molokai/
-
-#slime for copy paste to another screen 
-cd ~/.vim/bundle
-git clone git://github.com/jpalardy/vim-slime.git
-
-# Install vim ultisnips plugin
-cd ~/.vim/bundle/
-git clone https://github.com/SirVer/ultisnips.git
-git clone https://github.com/honza/vim-snippets.git
-echo -e "snippet php \"php\"\n<?php \${1} ?>\nendsnippet\n" >> ~/.vim/bundle/vim-snippets/UltiSnips/php.snippets # Add missing basic insert php snippet
-echo "extends html" >> ~/.vim/bundle/vim-snippets/UltiSnips/php.snippets #add html snippets to php files
 
 ##############################
 # DOTFILES 
