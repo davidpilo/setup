1aaf3c14691635159276169f8e5368ae663ad73f
David Pilo <davidpilo@gmail.com>
2014-05-29 19:01:08 +0200

    fix pathogen install

--- vim_setup.sh
+++ vim_setup.sh
@@ -2,7 +2,7 @@
 cd $HOME
 mkdir .vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+curl -LSso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 # Install tagbar
 cd ~/.vim/bundle
